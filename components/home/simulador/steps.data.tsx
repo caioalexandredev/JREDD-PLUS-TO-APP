f84f8db6234fe20ffc8d88864fc34f5df2b171e8
Caio Alexandre de Sousa Ramos <alexandre.caio.ramos@gmail.com>
2026-05-30 19:51:51 -0300

    Ajustando dados

--- components/home/simulador/steps.data.tsx
+++ components/home/simulador/steps.data.tsx
@@ -1,13 +1,9 @@
 const steps = [
   { label: "Identificação do proponente", status: "done" },
-  { label: "Experiência institucional", status: "done" },
   { label: "Caracterização do projeto", status: "done" },
   { label: "Localização e área", status: "current" },
   { label: "Público beneficiado", status: "pending" },
   { label: "Plano de execução", status: "pending" },
-  { label: "Metas e indicadores", status: "pending" },
-  { label: "Orçamento e financiamento", status: "pending" },
-  { label: "Evidências técnicas", status: "pending" },
   { label: "Declarações e submissão", status: "pending" },
 ]
 
